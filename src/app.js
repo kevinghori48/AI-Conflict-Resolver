844e0850df8bbf045035984147e124a9924e91d8
Anusi Patel <164149038+Anusi-Patel@users.noreply.github.com>
2025-12-29 10:38:22 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,24 +2,31 @@ import express from "express";
 import cors from "cors";
 import connectDB from "./config/db.js";
 import dotenv from "dotenv";
-dotenv.config();
 
+// Import Routes
 import authRoutes from "./routes/authRoutes.js";
 import reportRoutes from "./routes/reportRoutes.js";
 import chatRoutes from "./routes/chatRoutes.js";
-import disputeRoutes from "./routes/disputeRoutes.js";
+import smallDisputeRoutes from "./routes/smallDisputeRoutes.js";
+import officialDisputeRoutes from "./routes/officialDisputeRoutes.js";
+
+dotenv.config();
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
 connectDB();
 
+// Mount Routes
 app.use("/auth", authRoutes);
 app.use("/report", reportRoutes);
 app.use("/chat", chatRoutes);
-app.use("/dispute", disputeRoutes);
+app.use("/small-dispute", smallDisputeRoutes);
+app.use("/official-dispute", officialDisputeRoutes);
 
+// Simple root route to check if server is alive
 app.get("/", (req, res) => {
   res.send("API is working");
 });
