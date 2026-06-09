5ddea7dd3d2ea8e0f351736387966edbf0b96f49
HarshEscrow <harshgadhiya.escrowinfotech@gmail.com>
2026-06-09 12:56:48 +0530

    feat: initialize Google Generative AI client using GEMINI_API_KEY from environment variables

--- src/utils/gemini.js
+++ src/utils/gemini.js
@@ -1,5 +1,7 @@
+import "dotenv/config";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+console.log(process.env.GEMINI_API_KEY)
 
 export default genAI;
